38edca865125afda0d2822d319f3bf4255af9be2
Manoel Duran <manoel.duran@hotmail.com>
2021-12-23 18:00:39 -0300

    header profile page

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import PeopleSvg from '../../assets/people.svg';
 import { BackButton } from "../../components/BackButton";
 import { Feather } from '@expo/vector-icons';
 import {
     Container,
     Header,
+    HeaderTop,
     HeaderText,
     LogoutButton,
+    PhotoContainer,
+    Photo,
+    ButtonContainer,
+    ChangePhoto
 } from './styles';
 import { useTheme } from "styled-components";
+import { useNavigation } from "@react-navigation/native";
 
 export function Profile() {
+    const [avatar, setAvatar] = useState(null);
     const theme = useTheme();
+    const navigation = useNavigation<any>();
+    function handleBack() {
+        navigation.goBack()
+    };
+    function handleLogout() {
+
+    }
     return (
         <Container>
             <Header>
-                <BackButton />
-                <HeaderText>Editar Perfil</HeaderText>
-                <LogoutButton>
-                    <Feather
-                        name="power"
-                        color={theme.colors.text_datails}
+                <HeaderTop>
+                    <BackButton
+                        color={theme.colors.shape}
+                        onPress={handleBack}
                     />
-                </LogoutButton>
+                    <HeaderText>Editar Perfil</HeaderText>
+                    <LogoutButton onPress={handleLogout}  >
+                        <Feather
+                            name="power"
+                            color={theme.colors.shape}
+                            size={24}
+                        />
+                    </LogoutButton>
+                </HeaderTop>
+                <PhotoContainer>
+                    <Photo source={{ uri: 'https://github.com/manoelduran.png' }} />
+                        <ChangePhoto>
+                            <Feather
+                                name="camera"
+                                color={theme.colors.shape}
+                                size={24}
+                            />
+                        </ChangePhoto>
+                </PhotoContainer>
             </Header>
         </Container>
     );
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,4 +1,5 @@
-import { BorderlessButton } from "react-native-gesture-handler";
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
@@ -8,19 +9,51 @@ background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
 export const Header = styled.View`
-flex-direction: row;
-align-items: flex-start;
-justify-content: space-between;
-padding: 50px 30px;
+align-items: center;
 width: 100%;
 height: 227px;
+padding: 0px 24px;
 background-color: ${({ theme }) => theme.colors.header};
 color: ${({ theme }) => theme.colors.main};
 font-family: ${({ theme }) => theme.fonts.secundary_500};
+`;
+export const HeaderTop = styled.View`
+width: 100%;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+margin-top: ${getStatusBarHeight() + 30}px;
 `;
 export const HeaderText = styled.Text`
 font-size: ${RFValue(15)}px;
 font-family: ${({ theme }) => theme.fonts.primary_400};
 color: ${({ theme }) => theme.colors.background_secundary};
 `;
-export const LogoutButton = styled(BorderlessButton)``;
+export const LogoutButton = styled(BorderlessButton)`
+`;
+
+export const PhotoContainer = styled.View`
+align-items: flex-end;
+margin-top: 48px;
+width: 180px;
+height: 180px;
+border-radius: 90px; 
+background-color: ${({ theme }) => theme.colors.shape};
+`;
+
+export const Photo = styled.Image`
+width: 180px;
+height: 180px;
+border-radius: 90px; 
+`;
+export const ChangePhoto = styled(RectButton)`
+justify-content: center;
+align-items: center;
+position: absolute;
+bottom: 10px;
+right: 10px;
+width: 40px;
+height: 40px;
+background-color: ${({theme}) => theme.colors.main};
+`;
+
