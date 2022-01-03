b55ddecc1badadb96f145fcdf01583df0818958b
Manoel Duran <manoel.duran@hotmail.com>
2022-01-03 10:43:28 -0300

    app finalizado

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,44 +1,56 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import { FlatList, StatusBar } from 'react-native';
-import { useTheme } from 'styled-components';
+import { useTheme } from 'styled-components/native';
 import { AntDesign } from '@expo/vector-icons';
 import { BackButton } from '../../components/BackButton';
 import { Car } from '../../components/Car';
+import { format, parseISO } from 'date-fns';
 import { api } from '../../services/api';
+import { Car as ModelCar } from '../../databases/model/Car';
 import { Container, Header, Title, HeaderText, Content, Appointments, AppointmentTitle, AppointmentQuantify, CarWrapper, CarFooter, CarFooterTitle, CarFooterPeriod, CarFooterDate } from './styles';
 import { LoadAnimation } from '../../components/LoadAnimation';
 
-interface CarProps {
-    car: Car;
+
+
+interface DataProps {
     id: string;
-    user_id: string;
-    startDate: string;
-    endDate: string;
+    car: ModelCar;
+    start_date: string;
+    end_date: string;
 }
 
 export function MyCars() {
-    const [cars, setCars] = useState<CarProps[]>([])
-    const [loading, setLoading] = useState(true)
+    const [cars, setCars] = useState<DataProps[]>([]);
+    const [loading, setLoading] = useState(true);
+    const screenIsFocus = useIsFocused();
     const navigation = useNavigation<any>();
     const theme = useTheme();
     function handleBack() {
-        navigation.goBack()
+        navigation.goBack();
     };
 
     useEffect(() => {
         async function searchMyCars() {
             try {
-                const response = await api.get(`/schedules_byuser?user_id=1`)
-                setCars(response.data)
+                const response = await api.get(`/rentals`);
+                const dataFormatted = await response.data((data: DataProps) => {
+                    return {
+                        id: data.id,
+                        car: data.car,
+                        start_date: format(parseISO(data.start_date), 'dd/MM/yyyy'),
+                        end_date: format(parseISO(data.end_date), 'dd/MM/yyyy'),
+                    }
+                });
+                setCars(dataFormatted);
             } catch (err) {
-                console.log(err)
+                console.log(err);
             } finally {
-                setLoading(false)
+                setLoading(false);
             }
         }
-        searchMyCars()
-    }, [])
+        searchMyCars();
+    }, [screenIsFocus]);
     return (
         <Container>
             <Header>
@@ -73,14 +85,14 @@ export function MyCars() {
                                 <CarFooter>
                                     <CarFooterTitle>Periodo</CarFooterTitle>
                                     <CarFooterPeriod>
-                                        <CarFooterDate>{item.startDate}</CarFooterDate>
+                                        <CarFooterDate>{item.start_date}</CarFooterDate>
                                         <AntDesign
                                             name="arrowright"
                                             size={20}
                                             color={theme.colors.title}
                                             style={{ marginHorizontal: 10 }}
                                         />
-                                        <CarFooterDate>{item.endDate}</CarFooterDate>
+                                        <CarFooterDate>{item.end_date}</CarFooterDate>
                                     </CarFooterPeriod>
                                 </CarFooter>
                             </CarWrapper>
